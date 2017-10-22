c1504484dffafdbca42cace578e3f68e38938ea1
Joseph Lim <joseph.d.lim@gmail.com>
2017-10-22 18:18:24 +0000

    Fix Association Name

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ApplicationRecord
   has_one :messages_sender, :dependent => :destroy
   has_one :sender, :class_name => "User", through: :messages_sender, source: :user
   has_many :messages_recipients, :dependent => :destroy
-  has_many :recipients, :class_name => "User", through: :message_recipients, source: :user
+  has_many :recipients, :class_name => "User", through: :messages_recipients, source: :user
   accepts_nested_attributes_for :messages_recipients
 end
