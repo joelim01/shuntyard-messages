0d8d4ea17808dfae61459d9547f2dc7aa015f812
Joseph Lim <joseph.d.lim@gmail.com>
2017-10-22 17:56:37 +0000

    Namespace API versioned routes, Add User/Messages Controller

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,37 @@
+module Api
+  module V1
+    class MessagesController < ApplicationController
+      before_filter: :set_user, :set_messages
+
+            def index
+            end
+
+            def show
+            end
+
+            def new
+            end
+
+            def edit
+            end
+
+            def create
+            end
+
+            def update
+            end
+
+            def destroy
+            end
+
+            private
+
+            def set_user
+            end
+
+            def set_messages
+            end
+
+          end
+        end
+      end
