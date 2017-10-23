3a096b9208594f1cf5570f9cca068f624fc7f704
Joseph Lim <joseph.d.lim@gmail.com>
2017-10-23 12:42:27 +0000

    Serialize and render all API errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
+  rescue_from ApiExceptions::BaseException, :with => :render_error_response
+
+  def render_error_response(error)
+    render json: error, serializer: ApiExceptionsSerializer, status: 200
+  end
+
 end
