03a444be532cc985ece40c1d384f325fd331f4e2
Joseph Lim <joseph.d.lim@gmail.com>
2017-10-23 16:41:54 +0000

    Update message generator to use message_params

--- app/models/message_generator.rb
+++ app/models/message_generator.rb
@@ -1,14 +1,14 @@
 class MessageGenerator
-  include ActiveModel::Serialization
+  include ActiveModel::Validations
   validates_with MessageGeneratorValidator
   attr_reader :message, :user_id, :recipients, :body, :subject, :send_on
 
-  def initialize(user_id:, recipients:, body:, subject:)
+  def initialize(user_id:, message_params:)
     @user_id = user_id
-    @recipients = recipients
-    @send_on = send_on
-    @body = body
-    @subject = subject
+    @recipients = message_params[:recipients]
+    @send_on =  message_params[:send_on]
+    @body =  message_params[:body]
+    @subject =  message_params[:subject]
     @message = Message.create(sender: user_id, recipients: recipients, send_on: send_on, body: body, subject: subject) if valid?
   end
 end
