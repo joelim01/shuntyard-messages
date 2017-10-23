5c564f8e8c3266017515a366412be767dacebfcf
Joseph Lim <joseph.d.lim@gmail.com>
2017-10-23 12:40:31 +0000

    Create Message generator validator

---
+++ app/models/concerns/message_generator_validator.rb
@@ -0,0 +1,19 @@
+class MessageGeneratorValidator < ActiveModel::Validator
+  def validate(record
+    validate_recipients
+    validate_body
+    validate_subject
+  end
+
+  def validate_subject
+    raise ApiExceptions::MessageError::MissingSubject.new if record.subject.blank?
+  end
+
+  def validate_recipients
+    raise ApiExceptions::MessageError::MissingRecipient.new if record.recipients.blank?
+  end
+
+  def validate_body
+    raise ApiExceptions::MessageError::MissingBody.new if record.body.blank?
+  end
+end
