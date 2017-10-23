bda882a0d5c1efe9d14a32161b731ea27505d960
Joseph Lim <joseph.d.lim@gmail.com>
2017-10-23 16:43:55 +0000

    Removing login errors

--- lib/api_exceptions/login_error/malformed_email.rb
+++
@@ -1,6 +0,0 @@
-module ApiExceptions  
-  class LoginError < ApiExceptions::BaseException
-    class MalformedEmail < ApiExceptions::PurchaseError
-    end
-  end
-end
--- lib/api_exceptions/login_error/unknown_user.rb
+++
@@ -1,6 +0,0 @@
-module ApiExceptions
-  class LoginError < ApiExceptions::BaseException
-    class UnknownUser < ApiExceptions::PurchaseError
-    end
-  end
-end
