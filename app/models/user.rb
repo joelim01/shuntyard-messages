40152764c7a01b852c62973cf5a5c1f27c5cbadd
Joseph Lim <joseph.d.lim@gmail.com>
2017-10-24 00:49:47 +0000

    Add email and username AR validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   has_many :received_messages, :through => :messages_recipients, :source=> :message
   has_many :messages_senders
   has_many :messages_recipients
-  validates :email, presence: true, email: true
+  validates :email, presence: true, email: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+
 end
